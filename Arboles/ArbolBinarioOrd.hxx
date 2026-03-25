7c24536252f8340cfd27cd872e3a4d13a4be6f2d
KALEL2006 <kalel0319@hotmail.com>
2026-03-24 22:17:50 -0500

    feat: plantillas de arbol binario ordenado

---
+++ Arboles/ArbolBinarioOrd.hxx
@@ -0,0 +1,73 @@
+#ifndef ARBOLBINARIOORD_HXX
+#define ARBOLBINARIOORD_HXX
+
+#include "ArbolBinarioOrd.h"
+#include <iterator>
+#include <iostream>
+
+template <class T>
+ArbolBinarioOrd<T>::ArbolBinarioOrd() {
+    
+}
+
+template <class T>
+ArbolBinarioOrd<T>::~ArbolBinarioOrd() {
+    
+}
+
+template <class T>
+bool ArbolBinarioOrd<T>::esVacio() {
+    
+}
+
+template <class T>
+T ArbolBinarioOrd<T>::datoRaiz() {
+    
+}
+
+template <class T>
+int ArbolBinarioOrd<T>::altura() {
+    
+}
+
+template <class T>
+int ArbolBinarioOrd<T>::tamano() {
+    
+}
+
+template <class T>
+bool ArbolBinarioOrd<T>::insertar(T val) {
+    
+}
+
+template <class T>
+bool ArbolBinarioOrd<T>::eliminar(T val) {
+    
+}
+
+template <class T>
+bool ArbolBinarioOrd<T>::buscar(T val) {
+    
+}
+
+template <class T>
+void ArbolBinarioOrd<T>::preOrden() {
+    
+}
+
+template <class T>
+void ArbolBinarioOrd<T>::inOrden() {
+    
+}
+
+template <class T>
+void ArbolBinarioOrd<T>::posOrden() {
+    
+}
+
+template <class T>
+void ArbolBinarioOrd<T>::nivelOrden() {
+    
+}
+
+#endif
---
+++ Arboles/NodoBinario.hxx
@@ -0,0 +1,58 @@
+#ifndef NODOBINARIO_HXX
+#define NODOBINARIO_HXX
+
+#include "NodoBinario.h"
+#include <iterator>
+#include <iostream>
+
+template <class T>
+NodoBinario<T>::NodoBinario() {
+    
+}
+
+template <class T>
+NodoBinario<T>::NodoBinario(T val) {
+    
+}
+
+template <class T>
+NodoBinario<T>::~NodoBinario() {
+    
+}
+
+template <class T>
+T NodoBinario<T>::obtenerDato() {
+    
+}
+
+template <class T>
+void NodoBinario<T>::fijarDato(T val) {
+    
+}
+
+template <class T>
+NodoBinario<T>* NodoBinario<T>::obtenerHijoIzq() {
+    
+}
+
+template <class T>
+NodoBinario<T>* NodoBinario<T>::obtenerHijoDer() {
+    
+}
+
+template <class T>
+void NodoBinario<T>::fijarHijoIzq(NodoBinario<T>* izq) {
+    
+}
+
+template <class T>
+void NodoBinario<T>::fijarHijoDer(NodoBinario<T>* der) {
+    
+}
+
+template <class T>
+bool NodoBinario<T>::esHoja() {
+    
+}
+
+#endif
